4da755363a2c4566213d35f52dacb9ee1df75f93
Brakku <52824455+Brakku@users.noreply.github.com>
2019-07-12 21:54:13 +1000

    Add files via upload

---
+++ MapGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public enum Drawmode { NoiseMap, ColourMap, Mesh};
+    public Drawmode drawMode;
+
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarety;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    public TerrianTypes[] regions;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarety, offset);
+
+        Color[] coloutMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++) {
+            for (int x = 0; x < mapChunkSize; x++) {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++) {
+                    if (currentHeight <= regions[i].HeightValue) {
+                        coloutMap[y * mapChunkSize + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == Drawmode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == Drawmode.ColourMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(coloutMap, mapChunkSize, mapChunkSize));
+        }
+        else if (drawMode == Drawmode.Mesh) {
+            display.DrawMesh(MeshGenerator.GenerateTerrianMesh (noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(coloutMap, mapChunkSize, mapChunkSize));
+        }
+    }
+
+    void OnValidate() {
+        if (lacunarety < 1) {
+            lacunarety = 1;
+        }
+        if (octaves < 0) {
+            octaves = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrianTypes {
+    public string name;
+    public float HeightValue;
+    public Color colour;
+}
---
+++ Noise.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Noise {
+
+    public static float[,] GenerateNoiseMap(int MapWidth, int MapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+        float[,] noiseMap = new float[MapWidth, MapHeight];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            float offSetX = prng.Next(-100000, 100000) + offset.x;
+            float offSetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offSetX, offSetY);
+        }
+
+        if (scale <= 0 ) {
+            scale = 0.0001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = MapWidth / 2f;
+        float halfHeight = MapHeight / 2f;
+
+        for (int y = 0; y < MapHeight; y++) {
+            for (int x = 0; x < MapWidth; x++) {
+
+                float amplitude = 1;
+                float frequancy = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x - halfWidth) / scale * frequancy + octaveOffsets[i].x;
+                    float sampleY = (y - halfHeight) / scale * frequancy + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequancy *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight) {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight) {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < MapHeight; y++)
+        {
+            for (int x = 0; x < MapWidth; x++)
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            {
+                
+            }
+        }
+
+        return noiseMap;
+    }
+
+}
